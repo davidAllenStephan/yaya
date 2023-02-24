6510c6b45378981af967a66d0fe4877c14a2a14c
David Marino <david.marino@bayer.com>
2023-02-24 10:16:24 -0600

    asd

--- createAccount.js
+++ createAccount.js
@@ -1,4 +1,5 @@
 import { collection, addDoc } from 'firebase/firestore'
+import db from './firebaseConfig'
 const createAccount = async (name, secret, address) => {
 	await addDoc(collection(db, 'users'), {
 		secret: secret,
